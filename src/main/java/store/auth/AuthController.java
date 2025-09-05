1be0f809def2a4fc295a87ae6480eeccdd76bdf0
Humberto Sandmann <hsandmann@ieee.org>
2025-09-05 15:52:33 -0300

    openfeign

--- src/main/java/store/auth/AuthController.java
+++ src/main/java/store/auth/AuthController.java
@@ -1,9 +1,11 @@
 package store.auth;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@FeignClient(name = "auth", url = "http://auth:8080")
 public interface AuthController {
     
     @PostMapping("/auth/register")
